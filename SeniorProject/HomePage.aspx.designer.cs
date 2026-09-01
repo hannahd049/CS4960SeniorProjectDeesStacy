68151ec5f9dbf82b96870f7b79ea6ec401728486
hannahd049 <HGDees01@indianatech.net>
2026-09-01 13:00:18 -0400

    bunch of random styling css

--- SeniorProject/HomePage.aspx.designer.cs
+++ SeniorProject/HomePage.aspx.designer.cs
@@ -22,32 +22,5 @@ namespace SeniorProject
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// greeting control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label greeting;
-
-        /// <summary>
-        /// loginbutton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button loginbutton;
-
-        /// <summary>
-        /// signupbutton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button signupbutton;
     }
 }
---
+++ SeniorProject/Welcome.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Welcome.aspx.cs" Inherits="SeniorProject.Welcome" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="StyleSheet.css" />
+    <title>Login or Sign Up</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+            <div class="bubble-section">
+                <asp:Label ID="greeting" runat="server" Text="Please Login or Sign Up"></asp:Label> <br />
+                <asp:Button ID="loginbutton" runat="server" Text="Login" OnClick="loginbutton_Click" class="btn"></asp:Button><br />
+                <asp:Button ID="signupbutton" runat="server" Text="Sign Up" OnClick="signupbutton_Click" class="btn"></asp:Button><br />
+            </div>
+    </form>
+</body>
+</html>
